7983b1207e66bc2efa0699c95bb2df3d361101d4
yifan <1042750083@qq.com>
2022-06-13 17:30:56 +0800

    按钮位置调整

--- app/src/main/java/com/example/HCI_Project/RecordScreen.kt
+++ app/src/main/java/com/example/HCI_Project/RecordScreen.kt
@@ -1,8 +1,8 @@
 package com.example.HCI_Project
 
 import android.annotation.SuppressLint
+import android.text.Layout
 import android.util.Log
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,7 +11,21 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import android.view.View
+import android.widget.GridLayout
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import jp.live2d.sample.LAppLive2DManager
 import jp.live2d.sample.LAppLive2DManager.TAG
@@ -36,35 +50,62 @@ fun RecordBoard(
             launcher.launch("1")
         }
     }
-    Column {
-        text.value?.let { Greeting(it) }
+    Box(modifier = Modifier.fillMaxSize()){
+        Surface(shape = RectangleShape,
+            modifier = Modifier
+                .height(50.dp)
+                .fillMaxWidth()) {
+            Row() {
+                text.value?.let { Greeting(it) }
+                Button(
+                    onClick = {
+                        l2dmanager.changeModel()
+                    },
+                    ) {
+                    Text(text = "change")
+                }
+                Button(
+                    onClick = { navController.navigate("chatlist") },
+                    ) {
+                    Text(text = "chatpage")
+                }
+            }
+        }
+
+        Button(onClick = { viewModel.changePattern() },
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .offset(20.dp, -20.dp)
+                .clip(CircleShape)
+                .size(100.dp)
+        ) {
+            Text(text = "changePattern")
+        }
         Button(onClick = {
-            if(state_text=="start"){
+            if (state_text == "start") {
                 println("start")
                 viewModel.start()
-                state_text="stop"
+                state_text = "stop"
                 VoiceTTS.stop()
-            }
-            else if(state_text=="stop") {
+            } else if (state_text == "stop") {
                 println("stop")
 
                 viewModel.stop()
-                state_text="start"
+                state_text = "start"
                 //说完话增加模拟点击
-                SimulateTouch.simulateTouchEvent(l2dview, l2dview.width/2f,l2dview.height/2f)
+                SimulateTouch.simulateTouchEvent(
+                    l2dview,
+                    l2dview.width / 2f,
+                    l2dview.height / 2f
+                )
             }
-        }){
-                Text(text =state_text)
-        }
-        Button(onClick = {
-            l2dmanager.changeModel() }) {
-            Text(text = "change")
-        }
-        Button(onClick = { viewModel.changePattern() }) {
-            Text(text = "changePattern")
-        }
-        Button(onClick = { navController.navigate("chatlist") }) {
-            Text(text = "chatpage")
+        },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .offset(-20.dp, -20.dp)
+                .clip(CircleShape)
+                .size(80.dp)) {
+            Text(text = state_text)
         }
     }
 
