cc912ecde050c2b847ff962b2dfdbc3fd51aa063
LXY <1575337710@qq.com>
2022-06-13 13:38:48 +0800

    1

---
+++ live2D/build/generated/source/buildConfig/debug/jp/live2d/sample/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jp.live2d.sample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "jp.live2d.sample";
+  public static final String BUILD_TYPE = "debug";
+}
